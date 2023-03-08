072e87216a521792e2c6dbca1ecaff60b5498abe
JSPARK <55075290+ParkJae-sung@users.noreply.github.com>
2023-03-09 03:10:55 +0900

    fix: useeffect warning 해결 (#4)
    getClosesMetroStation을 useEffect로 구현하였을때 warning이 발생하고 반복적으로 해당 부분이 작동되기도 하므로, App에서 빼 function으로 따로 구현하였음

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+function getClosestMetroStation(latitude, longitude, metroStations) {
+  // 현재 위치와 가장 가까운 지하철 역을 찾는 함수
+  if (!latitude || !longitude) {
+    return null;
+  }
+
+  let closestStation = null;
+  let closestDistance = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 0; i < metroStations.length; i++) {
+    const distance = Haversine(
+      latitude,
+      longitude,
+      metroStations[i].lat,
+      metroStations[i].long
+    );
+    if (distance < closestDistance) {
+      closestStation = metroStations[i];
+      closestDistance = distance;
+    }
+  }
+  return closestStation;
+};
+
+function Haversine(lat1, lon1, lat2, lon2) {
+  //하버시안 공식을 이용하여 두 점 사이의 거리 구하기
+  const R = 6371e3; // 지구의 반지름 (m)
+  const φ1 = (lat1 * Math.PI) / 180;
+  const φ2 = (lat2 * Math.PI) / 180;
+  const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+  const Δλ = ((lon2 - lon1) * Math.PI) / 180;
+
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) *
+      Math.cos(φ2) *
+      Math.sin(Δλ / 2) *
+      Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c;
+  return d;
+};
 
 function App() {
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [metroStations, setMetroStations] = useState([]);
   const [closestStation, setClosestStation] = useState(null);
-  
-  const getClosestMetroStation = () => {
-    // 현재 위치와 가장 가까운 지하철 역을 찾는 함수
-    if (!latitude || !longitude) {
-      return null;
-    }
-
-    let closestStation = null;
-    let closestDistance = Number. MAX_SAFE_INTEGER;
-
-    for (let i = 0; i < metroStations.length; i++) {
-      const distance = Haversine(
-        latitude,
-        longitude,
-        metroStations[i].lat,
-        metroStations[i].long
-      );
-      if (distance < closestDistance) {
-        closestStation = metroStations[i];
-        closestDistance = distance;
-      }
-    }
-    return closestStation;
-  };
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -73,27 +91,8 @@ function App() {
       });
   }, []);
 
-  const Haversine = (lat1, lon1, lat2, lon2) => {
-    //하버시안 공식을 이용하여 두 점 사이의 거리 구하기
-    const R = 6371e3; // 지구의 반지름 (m)
-    const φ1 = (lat1 * Math.PI) / 180;
-    const φ2 = (lat2 * Math.PI) / 180;
-    const Δφ = ((lat2 - lat1) * Math.PI) / 180;
-    const Δλ = ((lon2 - lon1) * Math.PI) / 180;
-
-    const a =
-      Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-      Math.cos(φ1) *
-        Math.cos(φ2) *
-        Math.sin(Δλ / 2) *
-        Math.sin(Δλ / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const d = R * c;
-    return d;
-  };
-
   useEffect(() => {
-    setClosestStation(getClosestMetroStation());
+    setClosestStation(getClosestMetroStation(latitude, longitude, metroStations));
   }, [latitude, longitude, metroStations]);
 
   return (
