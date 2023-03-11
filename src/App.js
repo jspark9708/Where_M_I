a1ff98ec070bfe026b155fbe5bfe560cfa3a0ffd
JSPARK <55075290+ParkJae-sung@users.noreply.github.com>
2023-03-12 05:58:45 +0900

    style: 코드 가독성 좋게 정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function getClosestMetroStation(latitude, longitude, metroStations) {
   //현재 위치와 가장 가까운 지하철 역을 찾는 함수
@@ -23,7 +23,7 @@ function getClosestMetroStation(latitude, longitude, metroStations) {
     }
   }
   return closestStation;
-};
+}
 
 function Haversine(lat1, lon1, lat2, lon2) {
   //하버시안 공식을 이용하여 두 점 사이의 거리 구하기
@@ -35,14 +35,11 @@ function Haversine(lat1, lon1, lat2, lon2) {
 
   const a =
     Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-    Math.cos(φ1) *
-      Math.cos(φ2) *
-      Math.sin(Δλ / 2) *
-      Math.sin(Δλ/2);
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const d = R * c;
   return d;
-};
+}
 
 function App() {
   const [latitude, setLatitude] = useState(null);
@@ -62,21 +59,21 @@ function App() {
         }
       );
     } else {
-      console.log('Geolocation is not supported by this browser.');
+      console.log("Geolocation is not supported by this browser.");
     }
 
     //csv 파일에서 지하철 역 정보 가져오기
-    fetch('/metro.csv', {
+    fetch("/metro.csv", {
       headers: {
-        'Content-Type': 'text/csv; charset=UTF-8',
+        "Content-Type": "text/csv; charset=UTF-8",
       },
     })
       .then((response) => response.text())
       .then((data) => {
-        const rows = data.split('\n');
+        const rows = data.split("\n");
         //Structure declaration part of metroStation
         const metroStations = rows.reduce((result, row) => {
-          const columns = row.split(',');
+          const columns = row.split(",");
           const station = {
             line: columns[0],
             id: columns[1],
@@ -92,7 +89,9 @@ function App() {
   }, []);
 
   useEffect(() => {
-    setClosestStation(getClosestMetroStation(latitude, longitude, metroStations));
+    setClosestStation(
+      getClosestMetroStation(latitude, longitude, metroStations)
+    );
   }, [latitude, longitude, metroStations]);
 
   return (
@@ -108,23 +107,29 @@ function App() {
           <p>Longitude: {longitude}</p>
         </div>
         <div>
-  <h1>Close Station</h1>
-  {closestStation && (
-    <div class="station_container">
-      {metroStations
-        .filter((station) => station.name === closestStation.name)
-        .map((station) => (
-          <div className={`circle line-${station.line}`} key={station.id}>
-            <p className="line-text">{closestStation.name}</p>
-            <p className="line-text">{station.line}</p>
-          </div>
-        ))}
-    </div>
-  )}
-</div>
+          <h1>Close Station</h1>
+          {closestStation && (
+            <div class="station_container">
+              {metroStations
+                .filter((station) => station.name === closestStation.name)
+                .map((station) => (
+                  <div
+                    className={`circle line-${station.line}`}
+                    key={station.id}
+                  >
+                    <p className="line-text">{closestStation.name}</p>
+                    <p className="line-text">{station.line}</p>
+                  </div>
+                ))}
+            </div>
+          )}
+        </div>
 
         <div className="btn_area">
-         <p className="belowDescription">역의 정확도는 사용자의 네트워크 상태에 따라 오차가 발생할 수 있습니다.</p>
+          <p className="belowDescription">
+            역의 정확도는 사용자의 네트워크 상태에 따라 오차가 발생할 수
+            있습니다.
+          </p>
         </div>
       </div>
     </div>
