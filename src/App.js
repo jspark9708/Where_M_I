696e0fab7b0b3d7618a588d680ed7db82df42a74
JSPARK <55075290+ParkJae-sung@users.noreply.github.com>
2023-03-31 19:49:41 +0900

    refoctor: 역 구하는 함수 분리 (#8)
    getClosestMetroStation,
    getOtherLineStation.
    getAdjacentStations 같은 역을 구하는 함수를 분리하였으며.
    그에따라 haversine의 위치도 바꾸었음

--- src/App.js
+++ src/App.js
@@ -1,81 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Location } from "./map.js";
-import haversine from "./function/haversine.js";
-
-function getClosestMetroStation(latitude, longitude, metroStations) {
-  //현재 위치와 가장 가까운 지하철 역을 찾는 함수
-  if (!latitude || !longitude) {
-    return null;
-  }
-
-  let closestStation = null;
-  let closestDistance = Number.MAX_SAFE_INTEGER;
-
-  for (let i = 0; i < metroStations.length; i++) {
-    const distance = haversine(
-      latitude,
-      longitude,
-      metroStations[i].lat,
-      metroStations[i].long
-    );
-    if (distance < closestDistance) {
-      closestStation = metroStations[i];
-      closestDistance = distance;
-    }
-  }
-  return closestStation;
-}
-
-function getOtherLineStation(closestStation, metroStations) {
-  //환승역(역 명은 같으나, 호선이 다른경우)을 구해 반환하는 함수
-  if (!closestStation) {
-    //closestStation의 값이 없을때 함수를 더이상 진행하지 않을것
-    return [];
-  }
-
-  const otherStation = [];
-
-  for (let i = 0; i < metroStations.length; i++) {
-    if (
-      closestStation.name === metroStations[i].name &&
-      closestStation.line !== metroStations[i].line
-    ) {
-      otherStation.push(metroStations[i]);
-    }
-  }
-  return otherStation;
-}
-
-function getAdjacentStations(station, metroStations) {
-  //이전역, 다음역 구해 반환하는 함수
-  const adjacentStations = {
-    previousStation: null,
-    nextStation: null,
-  };
-  let found = false;
-  for (let i = 0; i < metroStations.length; i++) {
-    if (
-      metroStations[i].name === station.name &&
-      metroStations[i].line === station.line
-    ) {
-      found = true;
-    } else if (
-      found &&
-      metroStations[i].name === station.name &&
-      metroStations[i].line !== station.line
-    ) {
-      break;
-      //이전역, 다음역이 존재하지 않는 경우의 처리
-    } else if (found && metroStations[i].line === station.line) {
-      adjacentStations.nextStation = metroStations[i];
-      break;
-    } else if (metroStations[i].line === station.line) {
-      adjacentStations.previousStation = metroStations[i];
-    }
-  }
-  return adjacentStations;
-}
+import { getClosestMetroStation } from "./function/getinfo.js";
+import { getOtherLineStation } from "./function/getinfo.js";
+import { getAdjacentStations } from "./function/getinfo.js";
 
 function App() {
   const [latitude, setLatitude] = useState(null);
